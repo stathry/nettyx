e2b4451c607f5a871e4e11e22819afc2ea7a7aca
dongdaiming <dongdaiming@socian.com.cn>
2018-04-10 18:03:03 +0800

    netty

---
+++ src/test/java/org/stathry/nettyx/NettyServerTest.java
@@ -0,0 +1,60 @@
+package org.stathry.nettyx;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.junit.Test;
+
+/**
+ * 丢弃任何进入的数据
+ */
+public class NettyServerTest {
+
+    public void run(int port,ChannelHandler channelHandler) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(channelHandler);
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // 等待服务器  socket 关闭 。
+            // 在这个例子中，这不会发生，但你可以优雅地关闭你的服务器。
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    @Test
+    public void testDiscardServer() throws Exception {
+        int port = 8080;
+        run(port, new DiscardServerHandler());
+    }
+
+    @Test
+    public void testDirectServer() throws Exception {
+        int port = 8090;
+        run(port, new DirectServerHandler());
+    }
+
+}
